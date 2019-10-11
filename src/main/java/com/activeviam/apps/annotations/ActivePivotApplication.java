b0c26161b6e63d7679e71b2957e5f51b544ff41d
Marco Fabiani <mfa@activeviam.com>
2019-10-11 15:10:21 +0100

    Cleaned unused imports

--- src/main/java/com/activeviam/apps/annotations/ActivePivotApplication.java
+++ src/main/java/com/activeviam/apps/annotations/ActivePivotApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.stereotype.Component;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
