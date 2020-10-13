b54a107f92b31eb196e0989d585ebfd7e48433ce
Sam Brown <sbr@activeviam.com>
2020-10-13 21:08:00 +0100

    Minor comments and tidy up

--- src/main/java/com/activeviam/apps/cfg/pivot/CubeConfig.java
+++ src/main/java/com/activeviam/apps/cfg/pivot/CubeConfig.java
@@ -4,10 +4,13 @@ import com.activeviam.apps.constants.StoreAndFieldConstants;
 import com.activeviam.desc.build.ICanBuildCubeDescription;
 import com.activeviam.desc.build.ICubeDescriptionBuilder;
 import com.activeviam.desc.build.dimensions.ICanStartBuildingDimensions;
+import com.quartetfs.biz.pivot.context.impl.QueriesTimeLimit;
 import com.quartetfs.biz.pivot.cube.dimension.IDimension;
 import com.quartetfs.biz.pivot.cube.hierarchy.ILevelInfo;
 import com.quartetfs.biz.pivot.definitions.IActivePivotInstanceDescription;
 import com.quartetfs.fwk.ordering.impl.ReverseOrderComparator;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.activeviam.apps.cfg.pivot.PivotManagerConfig.INT_FORMATTER;
 import static com.activeviam.apps.cfg.pivot.PivotManagerConfig.NATIVE_MEASURES;
@@ -37,8 +40,10 @@ public class CubeConfig {
                 .jit()
 
                 // Shared context values
-                // Query maximum execution time (before timeout cancellation): 1h
+                // Query maximum execution time (before timeout cancellation): 30s
+                .withSharedContextValue(QueriesTimeLimit.of(30, TimeUnit.SECONDS))
                 .withSharedMdxContext().aggressiveFormulaEvaluation(true).end()
+
                 .withSharedDrillthroughProperties()
                 .withMaxRows(10000)
                 .end()
@@ -59,11 +64,13 @@ public class CubeConfig {
                 .withSingleLevelDimensions(
                         StoreAndFieldConstants.TRADES__TRADEID
                 )
-                .withDimension(StoreAndFieldConstants.ASOFDATE)
-                .withType(IDimension.DimensionType.TIME)
-                .withHierarchy(StoreAndFieldConstants.ASOFDATE).slicing()
-                .withLevelOfSameName()
-                .withType(ILevelInfo.LevelType.TIME).withComparator(ReverseOrderComparator.type)
+
+                // Make the AsOfDate hierarchy slicing - we do not aggregate across dates
+                // Also show the dates in reverse order ie most recent date first
+                .withDimension(StoreAndFieldConstants.ASOFDATE).withType(IDimension.DimensionType.TIME)
+                    .withHierarchy(StoreAndFieldConstants.ASOFDATE).slicing()
+                    .withLevelOfSameName()
+                        .withType(ILevelInfo.LevelType.TIME).withComparator(ReverseOrderComparator.type)
                 ;
 
     }
