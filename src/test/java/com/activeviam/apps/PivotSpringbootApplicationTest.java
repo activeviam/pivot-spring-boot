de76efd1e133d7d83352f72756363a1daa0870fd
Marco Fabiani <mfa@activeviam.com>
2021-11-02 12:28:21 +0000

    Bump AP version and use Junit 5

--- src/test/java/com/activeviam/apps/PivotSpringbootApplicationTest.java
+++ src/test/java/com/activeviam/apps/PivotSpringbootApplicationTest.java
@@ -1,20 +1,21 @@
 package com.activeviam.apps;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit4.SpringRunner;
 
+import static com.qfs.QfsWebUtils.url;
+import static com.qfs.server.cfg.impl.ActivePivotRestServicesConfig.PING_SUFFIX;
+import static com.qfs.server.cfg.impl.ActivePivotRestServicesConfig.REST_API_URL_PREFIX;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = PivotSpringBootApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
-public class PivotSpringbootApplicationTest {
+class PivotSpringbootApplicationTest {
 
 	@LocalServerPort
 	private int port;
@@ -22,19 +23,19 @@ public class PivotSpringbootApplicationTest {
 	@Autowired
 	private TestRestTemplate restTemplate;
 
-	@Before
+	@BeforeEach
 	public void setupAuth() {
 		restTemplate = restTemplate.withBasicAuth("admin", "admin");
 	}
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 	@Test
-	public void activePivotPingReturnsPong() {
-		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/pivot/rest/v5/ping",
-				String.class)).contains("pong");
+	void activePivotPingReturnsPong() {
+		var pingUrl = url("http://localhost:" + port, REST_API_URL_PREFIX, PING_SUFFIX);
+		assertThat(this.restTemplate.getForObject(pingUrl, String.class)).contains("pong");
 	}
 
 }
