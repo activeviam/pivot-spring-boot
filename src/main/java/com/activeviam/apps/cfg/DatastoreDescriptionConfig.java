986751384490db97ebec6d7bb1a5ecf0accd1bc8
Marco Fabiani <mfa@activeviam.com>
2022-05-09 10:19:48 +0100

    add categories datastore

--- src/main/java/com/activeviam/apps/cfg/DatastoreDescriptionConfig.java
+++ src/main/java/com/activeviam/apps/cfg/DatastoreDescriptionConfig.java
@@ -1,13 +1,5 @@
 package com.activeviam.apps.cfg;
 
-import static com.qfs.literal.ILiteralType.DOUBLE;
-import static com.qfs.literal.ILiteralType.LOCAL_DATE;
-import static com.qfs.literal.ILiteralType.STRING;
-
-import java.util.Collection;
-import java.util.Collections;
-import java.util.LinkedList;
-
 import com.activeviam.apps.constants.StoreAndFieldConstants;
 import com.qfs.desc.IDatastoreSchemaDescription;
 import com.qfs.desc.IReferenceDescription;
@@ -15,34 +7,55 @@ import com.qfs.desc.IStoreDescription;
 import com.qfs.desc.impl.DatastoreSchemaDescription;
 import com.qfs.desc.impl.StoreDescriptionBuilder;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+
+import static com.qfs.literal.ILiteralType.*;
+
 public class DatastoreDescriptionConfig {
 
-	public static IStoreDescription createTradesStoreDescription() {
-		return new StoreDescriptionBuilder().withStoreName(StoreAndFieldConstants.TRADES_STORE_NAME)
-				.withField(StoreAndFieldConstants.ASOFDATE, LOCAL_DATE).asKeyField()
-				.withField(StoreAndFieldConstants.TRADES_TRADEID, STRING).asKeyField()
-				.withField(StoreAndFieldConstants.TRADES_NOTIONAL, DOUBLE)
-				.build();
-	}
+    public static IStoreDescription createTradesStoreDescription() {
+        return new StoreDescriptionBuilder()
+                .withStoreName(StoreAndFieldConstants.TRADES_STORE_NAME)
+                .withField(StoreAndFieldConstants.ASOFDATE, LOCAL_DATE)
+                .asKeyField()
+                .withField(StoreAndFieldConstants.TRADES_TRADEID, STRING)
+                .asKeyField()
+                .withField(StoreAndFieldConstants.TRADES_NOTIONAL, DOUBLE)
+                .build();
+    }
 
-	public static Collection<IReferenceDescription> references() {
-		return Collections.emptyList();
-	}
+    public static IStoreDescription createCategoriesStoreDescription() {
+        return new StoreDescriptionBuilder()
+                .withStoreName(StoreAndFieldConstants.CATEGORIES_STORE_NAME)
+                .withField(StoreAndFieldConstants.ASOFDATE, LOCAL_DATE)
+                .asKeyField()
+                .withField(StoreAndFieldConstants.TRADES_TRADEID, STRING)
+                .asKeyField()
+                .withField(StoreAndFieldConstants.GROUP_ID, STRING)
+                .build();
+    }
 
-	/**
-	 *
-	 * Provide the schema description of the datastore.
-	 * <p>
-	 * It is based on the descriptions of the stores in the datastore, the descriptions of the references between those stores, and the optimizations and
-	 * constraints set on the schema.
-	 *
-	 * @return schema description
-	 */
-	public static IDatastoreSchemaDescription schemaDescription() {
+    public static Collection<IReferenceDescription> references() {
+        return Collections.emptyList();
+    }
 
-		final Collection<IStoreDescription> stores = new LinkedList<>();
-		stores.add(createTradesStoreDescription());
+    /**
+     *
+     * Provide the schema description of the datastore.
+     * <p>
+     * It is based on the descriptions of the stores in the datastore, the descriptions of the references between those stores, and the optimizations and
+     * constraints set on the schema.
+     *
+     * @return schema description
+     */
+    public static IDatastoreSchemaDescription schemaDescription() {
 
-		return new DatastoreSchemaDescription(stores, references());
-	}
+        final Collection<IStoreDescription> stores = new LinkedList<>();
+        stores.add(createTradesStoreDescription());
+        stores.add(createCategoriesStoreDescription());
+
+        return new DatastoreSchemaDescription(stores, references());
+    }
 }
--- src/main/java/com/activeviam/apps/constants/StoreAndFieldConstants.java
+++ src/main/java/com/activeviam/apps/constants/StoreAndFieldConstants.java
@@ -5,8 +5,11 @@ public class StoreAndFieldConstants {
 	/*********************** Stores names **********************/
 	public static final String TRADES_STORE_NAME = "Trades";
 
+	public static final String CATEGORIES_STORE_NAME = "Categories";
+
 	/********************* Stores fields ***********************/
 	public static final String ASOFDATE = "AsOfDate";
 	public static final String TRADES_TRADEID = "TradeID";
 	public static final String TRADES_NOTIONAL = "Notional";
+	public static final String GROUP_ID = "GroupId";
 }
