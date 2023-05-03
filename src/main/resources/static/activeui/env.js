0b1e4ca5deb5849700ad5d27aff007ea91c59774
Tharun John <tjo@activeviam.com>
2023-05-03 11:30:20 -0400

    project version aligned with ap version

--- src/main/resources/static/activeui/env.js
+++ src/main/resources/static/activeui/env.js
@@ -6,7 +6,7 @@ window.env = {
   // WARNING: Changing the keys of activePivotServers will break previously saved widgets and dashboards.
   // If you must do it, then you also need to update each one's serverKey attribute on your content server.
   activePivotServers: {
-    "pivot-springboot": {
+    "pivot-spring-boot": {
       url: baseUrl,
       version: "6.0.4",
     },
