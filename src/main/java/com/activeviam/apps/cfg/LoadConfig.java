1b0733801376191613a89270066ee6cc5ea48dd1
Tin Htun <tht@activeviam.com>
2023-07-07 09:01:25 +0800

    Postpone initial data loading until application started and ready.

---
+++ src/main/java/com/activeviam/apps/cfg/LoadConfig.java
@@ -0,0 +1,97 @@
+package com.activeviam.apps.cfg;
+
+import static com.activeviam.apps.cfg.SourceConfig.TRADES_TOPIC;
+
+import com.activeviam.apps.constants.StoreAndFieldConstants;
+import com.qfs.gui.impl.JungSchemaPrinter;
+import com.qfs.msg.IMessageChannel;
+import com.qfs.msg.csv.IFileInfo;
+import com.qfs.msg.csv.ILineReader;
+import com.qfs.msg.csv.impl.CSVSource;
+import com.qfs.source.impl.CSVMessageChannelFactory;
+import com.qfs.store.IDatastore;
+import com.qfs.store.impl.SchemaPrinter;
+import com.qfs.util.timing.impl.StopWatch;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collection;
+import javax.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+
+@Slf4j
+@Configuration
+public class LoadConfig {
+    private final IDatastore datastore;
+    private final CSVSource<Path> csvSource;
+    private final CSVMessageChannelFactory<Path> csvChannelFactory;
+    private final boolean schemaPrinter;
+    private final boolean postponeInitialLoad;
+
+    public LoadConfig(
+            IDatastore datastore,
+            CSVSource<Path> csvSource,
+            CSVMessageChannelFactory<Path> csvChannelFactory,
+            @Value("${schema.printer:true}") boolean schemaPrinter,
+            @Value("${initial-load.postpone:false}") boolean postponeInitialLoad) {
+        this.datastore = datastore;
+        this.csvSource = csvSource;
+        this.csvChannelFactory = csvChannelFactory;
+        this.schemaPrinter = schemaPrinter;
+        this.postponeInitialLoad = postponeInitialLoad;
+    }
+
+    @PostConstruct
+    void onLoadConfigReady() throws Exception {
+        log.info("onLoadConfigReady triggered");
+        if (!postponeInitialLoad) {
+            initialLoad();
+        }
+    }
+
+    @EventListener(value = ApplicationReadyEvent.class)
+    void onApplicationReady() throws Exception {
+        log.info("ApplicationReadyEvent triggered");
+        if (postponeInitialLoad) {
+            initialLoad();
+        }
+    }
+
+    void initialLoad() throws Exception {
+        log.info("Initial data load started.");
+        final Collection<IMessageChannel<IFileInfo<Path>, ILineReader>> csvChannels = new ArrayList<>();
+        csvChannels.add(csvChannelFactory.createChannel(TRADES_TOPIC, StoreAndFieldConstants.TRADES_STORE_NAME));
+
+        // do the transactions
+        final var before = System.nanoTime();
+
+        datastore.edit(t -> {
+            csvSource.fetch(csvChannels);
+            t.forceCommit();
+        });
+
+        final var elapsed = System.nanoTime() - before;
+        log.info("Initial data load completed in {} ms.", elapsed / 1000000L);
+
+        printStoreSizes();
+    }
+
+    private void printStoreSizes() {
+        // add some logging
+        if (schemaPrinter) {
+            // display the graph
+            System.setProperty("java.awt.headless", "false");
+            new JungSchemaPrinter(false).print("Datastore", datastore);
+        }
+
+        // Print stop watch profiling
+        StopWatch.get().printTimings();
+        StopWatch.get().printTimingLegend();
+
+        // print sizes
+        SchemaPrinter.printStoresSizes(datastore.getMostRecentVersion());
+    }
+}
--- src/main/java/com/activeviam/apps/cfg/SourceConfig.java
+++ src/main/java/com/activeviam/apps/cfg/SourceConfig.java
@@ -1,13 +1,24 @@
 package com.activeviam.apps.cfg;
 
+import com.activeviam.apps.constants.StoreAndFieldConstants;
+import com.qfs.gui.impl.JungSchemaPrinter;
+import com.qfs.msg.IMessageChannel;
+import com.qfs.msg.csv.ICSVParserConfiguration;
+import com.qfs.msg.csv.IFileInfo;
+import com.qfs.msg.csv.ILineReader;
+import com.qfs.msg.csv.filesystem.impl.FileSystemCSVTopicFactory;
+import com.qfs.msg.csv.impl.CSVParserConfiguration;
+import com.qfs.msg.csv.impl.CSVSource;
+import com.qfs.msg.csv.impl.CSVSourceConfiguration;
+import com.qfs.platform.IPlatform;
+import com.qfs.source.impl.CSVMessageChannelFactory;
+import com.qfs.store.IDatastore;
+import com.qfs.store.impl.SchemaPrinter;
+import com.qfs.util.timing.impl.StopWatch;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Properties;
-
-import com.qfs.msg.csv.impl.CSVSourceConfiguration;
-import com.qfs.platform.IPlatform;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,21 +26,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.core.env.Environment;
-
-import com.activeviam.apps.constants.StoreAndFieldConstants;
-import com.qfs.gui.impl.JungSchemaPrinter;
-import com.qfs.msg.IMessageChannel;
-import com.qfs.msg.csv.ICSVParserConfiguration;
-import com.qfs.msg.csv.ICSVSourceConfiguration;
-import com.qfs.msg.csv.IFileInfo;
-import com.qfs.msg.csv.ILineReader;
-import com.qfs.msg.csv.filesystem.impl.FileSystemCSVTopicFactory;
-import com.qfs.msg.csv.impl.CSVParserConfiguration;
-import com.qfs.msg.csv.impl.CSVSource;
-import com.qfs.source.impl.CSVMessageChannelFactory;
-import com.qfs.store.IDatastore;
-import com.qfs.store.impl.SchemaPrinter;
-import com.qfs.util.timing.impl.StopWatch;
 
 @Configuration
 public class SourceConfig {
@@ -89,25 +85,25 @@ public class SourceConfig {
 	 * **************************** Initial load *********************************
 	 */
 
-	@Bean
-	@DependsOn("startManager")
-	public Void initialLoad() {
-		final Collection<IMessageChannel<IFileInfo<Path>, ILineReader>> csvChannels = new ArrayList<>();
-		csvChannels.add(csvChannelFactory().createChannel(TRADES_TOPIC, StoreAndFieldConstants.TRADES_STORE_NAME));
-
-		// do the transactions
-		final var before = System.nanoTime();
-
-		datastore.edit(t -> {
-			csvSource().fetch(csvChannels);
-			t.forceCommit();
-		});
-		final var elapsed = System.nanoTime() - before;
-		logger.info("Initial data load completed in {} ms.", elapsed / 1000000L);
-
-		printStoreSizes();
-		return null;
-	}
+//	@Bean
+//	@DependsOn("startManager")
+//	public Void initialLoad() {
+//		final Collection<IMessageChannel<IFileInfo<Path>, ILineReader>> csvChannels = new ArrayList<>();
+//		csvChannels.add(csvChannelFactory().createChannel(TRADES_TOPIC, StoreAndFieldConstants.TRADES_STORE_NAME));
+//
+//		// do the transactions
+//		final var before = System.nanoTime();
+//
+//		datastore.edit(t -> {
+//			csvSource().fetch(csvChannels);
+//			t.forceCommit();
+//		});
+//		final var elapsed = System.nanoTime() - before;
+//		logger.info("Initial data load completed in {} ms.", elapsed / 1000000L);
+//
+//		printStoreSizes();
+//		return null;
+//	}
 
 	private ICSVParserConfiguration createParserConfig(final int columnCount, final List<String> columns) {
 		final var cfg = columns == null ? new CSVParserConfiguration(columnCount) : new CSVParserConfiguration(columns);
@@ -115,21 +111,5 @@ public class SourceConfig {
 		return cfg;
 	}
 
-	private void printStoreSizes() {
 
-		// add some logging
-		if (Boolean.parseBoolean(env.getProperty("schema.printer", "true"))) {
-			// display the graph
-			System.setProperty("java.awt.headless", "false");
-			new JungSchemaPrinter(false).print("Datastore", datastore);
-		}
-
-		// Print stop watch profiling
-		StopWatch.get().printTimings();
-		StopWatch.get().printTimingLegend();
-
-		// print sizes
-		SchemaPrinter.printStoresSizes(datastore.getMostRecentVersion());
-
-	}
 }
