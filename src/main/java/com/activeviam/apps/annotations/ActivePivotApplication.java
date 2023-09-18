722fdef921f784291359f7ba391b65ae8d55a7f7
Tin Htun <tht@activeviam.com>
2023-09-18 17:23:35 +0800

    Activate APM with minimal configuration.

--- src/main/java/com/activeviam/apps/annotations/ActivePivotApplication.java
+++ src/main/java/com/activeviam/apps/annotations/ActivePivotApplication.java
@@ -1,5 +1,8 @@
 package com.activeviam.apps.annotations;
 
+import com.activeviam.apm.cfg.impl.MonitoredDataLoadingConfig;
+import com.activeviam.apm.cfg.impl.MonitoringJmxConfig;
+import com.activeviam.apm.cfg.impl.QueryPerformanceEvaluatorConfig;
 import com.activeviam.properties.cfg.impl.ActiveViamPropertyFromSpringConfig;
 import com.activeviam.spring.config.activeui.ActiveUIResourceServerConfig;
 import com.activeviam.spring.config.adminui.AdminUIResourceServerConfig;
@@ -49,6 +52,11 @@ import org.springframework.context.annotation.Import;
 
             // Internationalization
             LocalI18nConfig.class,
+
+            // APM (https://docs.activeviam.com/products/atoti/server/latest/docs/monitoring/application_performance_monitoring/#monitored-spring-configuration)
+            MonitoringJmxConfig.class,
+            MonitoredDataLoadingConfig.class,
+            QueryPerformanceEvaluatorConfig.class,
 
             // Expose Admin UI web application
             AdminUIResourceServerConfig.class,
