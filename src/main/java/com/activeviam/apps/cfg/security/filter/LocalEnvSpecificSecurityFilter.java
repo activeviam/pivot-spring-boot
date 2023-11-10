46b9acd49e0d8cf2027934b6e993df3d8e06a882
Olivier Billiard <obi@activeviam.com>
2023-11-10 22:53:44 +0800

    Change the code to get the referer set by UI

--- src/main/java/com/activeviam/apps/cfg/security/filter/LocalEnvSpecificSecurityFilter.java
+++ src/main/java/com/activeviam/apps/cfg/security/filter/LocalEnvSpecificSecurityFilter.java
@@ -8,14 +8,29 @@ package com.activeviam.apps.cfg.security.filter;
 
 import static com.qfs.QfsWebUtils.url;
 
+import java.io.IOException;
+
 import javax.annotation.PostConstruct;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Component
+@Slf4j
 public class LocalEnvSpecificSecurityFilter implements IEnvSpecificSecurityFilter {
     public static final String COOKIE_NAME = "JSESSIONID";
     private static final String COOKIE_NAME_PROPERTY = "server.servlet.session.cookie.name";
@@ -39,6 +54,7 @@ public class LocalEnvSpecificSecurityFilter implements IEnvSpecificSecurityFilte
                         .deleteCookies(cookieName)
                         .invalidateHttpSession(true))
                 .formLogin()
+                .successHandler(new HeaderRefererAuthenticationSuccessHandler())
                 .and();
     }
 
@@ -50,6 +66,7 @@ public class LocalEnvSpecificSecurityFilter implements IEnvSpecificSecurityFilte
                         .deleteCookies(cookieName)
                         .invalidateHttpSession(true))
                 .formLogin()
+                .successHandler(new HeaderRefererAuthenticationSuccessHandler())
                 .permitAll()
                 .and();
     }
@@ -57,5 +74,36 @@ public class LocalEnvSpecificSecurityFilter implements IEnvSpecificSecurityFilte
     @Override
     public HttpSecurity applyExcelAuthentication(HttpSecurity http) throws Exception {
         return http.httpBasic().and();
+    }
+
+    private static class HeaderRefererAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+        private RequestCache requestCache = new HttpSessionRequestCache();
+
+        @Override
+        public void onAuthenticationSuccess(
+                HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+                throws ServletException, IOException {
+            var savedRequest = requestCache.getRequest(request, response);
+            if (savedRequest == null) {
+                super.onAuthenticationSuccess(request, response, authentication);
+                return;
+            }
+            var targetUrlParameter = getTargetUrlParameter();
+            if (isAlwaysUseDefaultTargetUrl()
+                    || (targetUrlParameter != null && StringUtils.hasText(request.getParameter(targetUrlParameter)))) {
+                requestCache.removeRequest(request, response);
+                super.onAuthenticationSuccess(request, response, authentication);
+                return;
+            }
+            clearAuthenticationAttributes(request);
+            // Use the DefaultSavedRequest URL
+            var referers = savedRequest.getHeaderValues(HttpHeaders.REFERER);
+            var targetUrl = !CollectionUtils.isEmpty(referers) ? referers.get(0) : savedRequest.getRedirectUrl();
+            getRedirectStrategy().sendRedirect(request, response, targetUrl);
+        }
+
+        public void setRequestCache(RequestCache requestCache) {
+            this.requestCache = requestCache;
+        }
     }
 }
