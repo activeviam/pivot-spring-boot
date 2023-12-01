8a84a9c8ccfa71699b44cb87caa10cea8e3a140a
Marco Fabiani <mfa@activeviam.com>
2023-12-01 17:25:42 +0000

    dates

--- src/main/java/com/activeviam/apps/data/load/DataLoadCustomisations.java
+++ src/main/java/com/activeviam/apps/data/load/DataLoadCustomisations.java
@@ -7,8 +7,10 @@
 
 package com.activeviam.apps.data.load;
 
+import static com.activeviam.apps.constants.StoreAndFieldConstants.ASOFDATE;
 import static com.activeviam.apps.constants.StoreAndFieldConstants.INSTRUMENTS_STORE_NAME;
 import static com.activeviam.apps.constants.StoreAndFieldConstants.SCOPE_CONSTANT;
+import static com.activeviam.apps.constants.StoreAndFieldConstants.TRADES_STORE_NAME;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +20,8 @@ import com.activeviam.data.load.DataLoadConfiguration;
 import com.activeviam.data.load.ScopedColumnCalculator;
 import com.activeviam.data.load.source.csv.CsvDatasourceConfiguration;
 import com.activeviam.data.load.source.csv.model.CsvStoresDataLoadConfigurer;
+import com.activeviam.fwk.ActiveViamRuntimeException;
+import com.qfs.msg.IColumnCalculator;
 
 @Configuration
 @Import({DataLoadConfiguration.class, CsvDatasourceConfiguration.class})
@@ -25,17 +29,33 @@ public class DataLoadCustomisations {
 
     public static final String CONSTANT_SCOPE_PARAMETER = "CONSTANT";
 
+    public static final String AS_OF_DATE_SCOPE_PARAMETER = "AS_OF_DATE";
+
+    private static IColumnCalculator asOfDateColumnCalculator() {
+        return new ScopedColumnCalculator<>(ASOFDATE, (context, scope) -> {
+            if (scope.hasParameter(AS_OF_DATE_SCOPE_PARAMETER)) {
+                return scope.get(AS_OF_DATE_SCOPE_PARAMETER);
+            } else {
+                throw new ActiveViamRuntimeException("AsOfDate missing from scope, cannot load data");
+            }
+        });
+    }
+
     @Bean
     Void csvDataLoadCustomisations(CsvStoresDataLoadConfigurer configurer) {
         configurer.configure(INSTRUMENTS_STORE_NAME, config -> config.editColumns(cols -> {
                     cols.remove(SCOPE_CONSTANT);
+                    cols.remove(ASOFDATE);
                     return cols;
                 })
                 .columnCalculator(new ScopedColumnCalculator<>(
-                        SCOPE_CONSTANT,
-                        (context, scope) -> scope.hasParameter(CONSTANT_SCOPE_PARAMETER)
-                                ? scope.get(CONSTANT_SCOPE_PARAMETER)
-                                : context.getValue(SCOPE_CONSTANT))));
+                        SCOPE_CONSTANT, (context, scope) -> scope.get(CONSTANT_SCOPE_PARAMETER)))
+                .columnCalculator(asOfDateColumnCalculator()));
+        configurer.configure(TRADES_STORE_NAME, config -> config.editColumns(cols -> {
+                    cols.remove(ASOFDATE);
+                    return cols;
+                })
+                .columnCalculator(asOfDateColumnCalculator()));
         return null;
     }
 }
