b3a6f019af157603d7f8cbcbdbe8678fde7ad76d
Woo Ming Kong <mwo@activeviam.com>
2024-03-08 18:46:51 +0800

    remove runTest

--- src/main/java/com/activeviam/apps/controllers/HelloController.java
+++ src/main/java/com/activeviam/apps/controllers/HelloController.java
@@ -96,59 +96,7 @@ public class HelloController {
         return new org.apache.hadoop.fs.Path(parquetDir.getPath());
     }
 
-
-    @GetMapping("/test")
-    public String runTest() {
-        final ITransactionManager transactionManager = datastore.getTransactionManager();
-        int count = 0;
-        while (count++ < 10) {
-            processLoading(count, transactionManager);
-        }
-        return "Run Test!";
-    }
-
-    private void processLoading(int cycle, ITransactionManager transactionManager) {
-        LOGGER.info("processing "+cycle);
-        try {
-            LOGGER.info("going to start transaction ");
-            transactionManager.startTransaction();
-
-            // add record
-            addRecord(LocalDate.of(2019, 1, 1), String.valueOf(cycle*10), transactionManager);
-
-            if (cycle > 7)
-                throw new ActiveViamRuntimeException("Exit Process Loading");
-            LOGGER.info("going to commit Transaction");
-            transactionManager.commitTransaction();
-        }
-        catch (Exception e) {
-            try {
-                LOGGER.info("going to rollback transaction");
-                transactionManager.rollbackTransaction();
-                LOGGER.info("successfully rolled back");
-            }
-            catch (Exception rollbackException) {
-                LOGGER.severe("Process Loading " + cycle + " is interrupted");
-                rollbackException.printStackTrace();
-            }
-        }
-    }
-
-    private void addRecord(LocalDate date, String tradeId, ITransactionManager transactionManager) {
-        Double notional = new Random().nextDouble() * (100D - 1D); // random number between 1 and 100
-        var record = new Trade(date, tradeId, notional);
-        transactionManager.add(StoreAndFieldConstants.TRADES_STORE_NAME, date, tradeId, notional);
-        LOGGER.info("added "+record.toString());
-    }
-
     @GetMapping("/hello")
     public String index() { return "Hello from Pivot Spring Boot!"; }
 
-    @AllArgsConstructor
-    @ToString
-    private static class Trade {
-        private final LocalDate date;
-        private final String tradeId;
-        private final Double notional;
-    }
 }
