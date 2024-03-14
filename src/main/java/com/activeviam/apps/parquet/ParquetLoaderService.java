b8d6543dffb5e3e851fcfb7ed33dcfb603dd7abf
Woo Ming Kong <mwo@activeviam.com>
2024-03-14 12:12:50 +0800

    load multiple files with executeTasks

--- src/main/java/com/activeviam/apps/parquet/ParquetLoaderService.java
+++ src/main/java/com/activeviam/apps/parquet/ParquetLoaderService.java
@@ -10,20 +10,27 @@ import com.qfs.store.NoTransactionException;
 import com.qfs.store.transaction.DatastoreTransactionException;
 import com.qfs.store.transaction.ITransactionManager;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class ParquetLoaderService {
 	private static final Logger LOGGER = Logger.getLogger(ParquetLoaderService.class.getSimpleName());
@@ -40,6 +47,7 @@ public class ParquetLoaderService {
 	private ExecutorService copyFilesService = null;
 	private volatile boolean isContinueCopyFiles = false;
 	private Random random = new Random();
+	private ExecutorService executorService = Executors.newFixedThreadPool(3);
 
 	public ParquetLoaderService(IDatastore datastore) {
 		this.datastore = datastore;
@@ -120,31 +128,7 @@ public class ParquetLoaderService {
 		final ITransactionManager transactionManager = datastore.getTransactionManager();
 		try {
 			transactionManager.startTransaction(StoreAndFieldConstants.TRADES_STORE_NAME);
-			final var storeName = StoreAndFieldConstants.TRADES_STORE_NAME;
-			final var storePath = new org.apache.hadoop.fs.Path(PARQUET_LOAD_FOLDER.resolve("trades03.parquet").toString());
-
-			// If the storePath provided is too high-level (parent level) e.g. bucket level,
-			// consider setting recursive to false to prevent dive diving into unwanted subdirectories
-//			LOGGER.info("Processing parquet file from [" + storePath + "]");
-			new ParquetParserBuilder(datastore)
-					.withNumberOfThreads(NUM_THREADS_PARSER)
-					.build()
-					.parse(storePath,
-							new Configuration(false),
-							(IParquetFieldParsers) null,
-							IStoreToParquetMapping.builder().onStore(storeName)
-									.feedStoreField(StoreAndFieldConstants.ASOFDATE)
-									.withColumnCalculator()
-									.fromSingleParquetField("AsOf")
-									.map(a -> LocalDate.parse((String) a, DateTimeFormatter.ofPattern("yyyy-MM-dd")))
-									.feedStoreField(StoreAndFieldConstants.TRADES__TRADEID)
-									.withParquetField("TradeId")
-									.feedStoreField(StoreAndFieldConstants.TRADES__NOTIONAL)
-									.withColumnCalculator()
-									.fromSingleParquetField("Notional")
-									.map(a -> Double.parseDouble((String) a))
-									.build()
-					);
+			processFiles("trades01.parquet", "trades02.parquet", "trades03.parquet");
 //			LOGGER.info("Commit Transaction");
 			transactionManager.commitTransaction();
 		} catch (DatastoreTransactionException e) {
@@ -179,9 +163,10 @@ public class ParquetLoaderService {
 				transactionManager.rollbackTransaction();
 //				LOGGER.info("successfully rolled back");
 			}
-			catch (Exception rollbackException) {
+			catch (DatastoreTransactionException rollbackException) {
 				LOGGER.severe("Rollback failed");
 				rollbackException.printStackTrace();
+				throw new ActiveViamRuntimeException("Failed to rollback", rollbackException);
 			}
 		} catch (IllegalStateException e) {
 			LOGGER.severe("IllegalStateException");
@@ -195,9 +180,92 @@ public class ParquetLoaderService {
 				LOGGER.severe("Rollback failed");
 				rollbackException.printStackTrace();
 			}
+		} catch (ExecutionException e) {
+			LOGGER.severe("ExecutionException");
+			e.printStackTrace();
+			try {
+//				LOGGER.info("going to rollback transaction");
+				transactionManager.rollbackTransaction();
+//				LOGGER.info("successfully rolled back");
+			}
+			catch (Exception rollbackException) {
+				LOGGER.severe("Rollback failed");
+				rollbackException.printStackTrace();
+			}
+		} catch (InterruptedException e) {
+			LOGGER.severe("InterruptedException");
+			e.printStackTrace();
+			try {
+//				LOGGER.info("going to rollback transaction");
+				transactionManager.rollbackTransaction();
+//				LOGGER.info("successfully rolled back");
+			}
+			catch (Exception rollbackException) {
+				LOGGER.severe("Rollback failed");
+				rollbackException.printStackTrace();
+			}
 		}
 	}
 
+	private void processFiles(String... filenames) throws IOException, ExecutionException, InterruptedException {
+		List<Callable<Void>> tasks = Arrays.stream(filenames)
+				.map(filename ->
+						new Callable<Void>() {
+							@Override
+							public Void call() throws Exception {
+								LOGGER.info("Processing parquet file [" + filename + "]");
+								final var storePath = new org.apache.hadoop.fs.Path(PARQUET_LOAD_FOLDER.resolve(filename).toString());
+
+								try {
+									new ParquetParserBuilder(datastore)
+											.withNumberOfThreads(NUM_THREADS_PARSER)
+											.build()
+											.parse(storePath,
+													new Configuration(false),
+													(IParquetFieldParsers) null,
+													IStoreToParquetMapping.builder().onStore(StoreAndFieldConstants.TRADES_STORE_NAME)
+															.feedStoreField(StoreAndFieldConstants.ASOFDATE)
+															.withColumnCalculator()
+															.fromSingleParquetField("AsOf")
+															.map(a -> LocalDate.parse((String) a, DateTimeFormatter.ofPattern("yyyy-MM-dd")))
+															.feedStoreField(StoreAndFieldConstants.TRADES__TRADEID)
+															.withParquetField("TradeId")
+															.feedStoreField(StoreAndFieldConstants.TRADES__NOTIONAL)
+															.withColumnCalculator()
+															.fromSingleParquetField("Notional")
+															.map(a -> Double.parseDouble((String) a))
+															.build()
+											);
+								} catch (IOException e) {
+									throw new ActiveViamRuntimeException(e);
+								}
+								return null;
+							}
+						}).collect(Collectors.toList());
+		executeTasks(tasks);
+	}
+
+	private void executeTasks(List<Callable<Void>> tasks) throws ExecutionException, InterruptedException {
+		if (tasks.isEmpty()) {
+			return;
+		}
+		CompletionService completionService = new ExecutorCompletionService<>(executorService);
+		List<Future> futures = new ArrayList<>(tasks.size());
+		try {
+			for (Callable<Void> task : tasks) {
+				futures.add(completionService.submit(task));
+			}
+			for (int i = 0; i < tasks.size(); i++) {
+				Future future = completionService.take();
+				future.get(); // throws exception if failed
+			}
+		} catch (RuntimeException | InterruptedException | ExecutionException e) {
+			for (Future future : futures) {
+				future.cancel(false);
+			}
+			throw e;
+		}
+	}
 
 	/**
 	 * Copy files from saved to load folder
